717b4d1fb6589ed9c5abcc918c6fa4a2c7920c89
Glen Pegado <glenpegado@gmail.com>
2019-02-13 15:31:12 -0800

    Final commit

---
+++ precache-manifest.97e68d4e1dd35e3ea92ce9edfd6b5429.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1867f9f949c8ca323808",
+    "url": "/static/css/main.6b2a5d43.chunk.css"
+  },
+  {
+    "revision": "1867f9f949c8ca323808",
+    "url": "/static/js/main.1867f9f9.chunk.js"
+  },
+  {
+    "revision": "b00646b1b7be99282608",
+    "url": "/static/js/1.b00646b1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ba46cbed315d4fb40f4dc9561eb692c",
+    "url": "/index.html"
+  }
+];
